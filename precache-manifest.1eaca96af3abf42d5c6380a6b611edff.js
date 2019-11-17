986da30fdf544e292863aa7e607a725207364854
HuyNT272 <tienhuy1995@gmail.com>
2019-11-17 16:52:42 +0700

    Updates

---
+++ precache-manifest.1eaca96af3abf42d5c6380a6b611edff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e7ef0741473635b7ee0cff7c82f0dfd",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "37b7d63e613cc672f0c5",
+    "url": "/monsters-rolodex/static/css/main.a2a77430.chunk.css"
+  },
+  {
+    "revision": "750af461b7e651b41621",
+    "url": "/monsters-rolodex/static/js/2.ef7711ad.chunk.js"
+  },
+  {
+    "revision": "37b7d63e613cc672f0c5",
+    "url": "/monsters-rolodex/static/js/main.9d59f907.chunk.js"
+  },
+  {
+    "revision": "e9f306bcf1c509cf0d18",
+    "url": "/monsters-rolodex/static/js/runtime-main.3e4d36ee.js"
+  }
+]);
